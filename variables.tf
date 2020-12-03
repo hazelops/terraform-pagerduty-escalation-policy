d7e6345a5be2582f3ca9e04806dd2726b165ec30
Artem Tyuev <drojfs@gmail.com>
2020-12-03 09:44:04 +0300

    fix: add repeat_loops

--- variables.tf
+++ variables.tf
@@ -10,6 +10,11 @@ variable "name" {
   description = "Name of escalation policy. Make it meaningful"
 }
 
+variable "repeat_loops" {
+  description = "The number of times the escalation policy will repeat after reaching the end of its escalation."
+  default = 2
+}
+
 variable "type" {
   description = "Can be user, schedule, user_reference or schedule_reference"
   default     = "schedule"
